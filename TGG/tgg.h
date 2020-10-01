79dc43265bcd2982ce49420b177ae652728b011c
Kanken6174 <49340711+Kanken6174@users.noreply.github.com>
2020-10-01 19:59:17 +0200

    Add files via upload
    First upload of TGG

---
+++ TGG/tgg.h
@@ -0,0 +1,11 @@
+void dterm(int* tw0, int* th0);
+void characterdatatest();
+void adjustWindowSize();
+int makeworld(int seed);
+int namesc(char*);
+
+void ZAWARUDOinit(char poulet);
+void pullrandom(char opt);
+
+
+
