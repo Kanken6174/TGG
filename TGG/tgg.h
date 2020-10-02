73d4b48ee2ba237cfc9fdd07948541ae767b7b37
Kanken6174 <onyxab3q@outlook.com>
2020-10-02 18:05:24 +0200

    Update tgg.h

--- TGG/tgg.h
+++ TGG/tgg.h
@@ -1,11 +1,14 @@
 void dterm(int* tw0, int* th0);
 void characterdatatest();
 void adjustWindowSize();
-int makeworld(int seed);
+
 int namesc(char*);
+//char dkawrite(const char *filename, int count)
 
 void ZAWARUDOinit(char poulet);
-void pullrandom(char opt);
+void pullrandom();
+void pick(char types, char* adress);
+
 
 
 
